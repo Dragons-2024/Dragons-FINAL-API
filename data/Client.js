ea4769e9e3533d7f6d7d78dff5be50b6a5934315
Lena Castillo <lenac@uninorte.edu.co>
2024-11-08 10:00:24 -0500

    clientes-2

--- data/Client.js
+++ data/Client.js
@@ -20,6 +20,39 @@ const clientes = [
       "telefono": "098-765-4321",
       "correoCorporativo": "info@companiaabc.com",
       "activo": false
+    },
+    {
+      "id": 3,
+      "nit": "112233445",
+      "nombre": "Tech Solutions Ltd.",
+      "direccion": "Calle Innovación 45",
+      "ciudad": "Tecnópolis",
+      "pais": "Innovalandia",
+      "telefono": "555-123-4567",
+      "correoCorporativo": "contacto@techsolutions.com",
+      "activo": true
+    },
+    {
+      "id": 4,
+      "nit": "556677889",
+      "nombre": "Global Enterprises S.A.",
+      "direccion": "Paseo del Comercio 123",
+      "ciudad": "Metropolis",
+      "pais": "Globalandia",
+      "telefono": "777-987-6543",
+      "correoCorporativo": "atencion@globalenterprises.com",
+      "activo": true
+    },
+    {
+      "id": 5,
+      "nit": "223344556",
+      "nombre": "Distribuciones ABC S.R.L.",
+      "direccion": "Calle de la Industria 999",
+      "ciudad": "Ciudad Industrial",
+      "pais": "Industriolandia",
+      "telefono": "222-333-4444",
+      "correoCorporativo": "ventas@distribucionesabc.com",
+      "activo": false
     }
   ];
   
