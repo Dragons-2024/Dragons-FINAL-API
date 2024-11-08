e9c53301683206ed052be831ea8d3f8e087f97fb
Lena Castillo <lenac@uninorte.edu.co>
2024-11-08 09:57:32 -0500

    clientes

---
+++ data/Client.js
@@ -0,0 +1,27 @@
+const clientes = [
+    {
+      "id": 1,
+      "nit": "123456789",
+      "nombre": "Empresa XYZ S.A.",
+      "direccion": "Calle Falsa 123",
+      "ciudad": "Ciudad Falsa",
+      "pais": "Falsolandia",
+      "telefono": "123-456-7890",
+      "correoCorporativo": "contacto@empresaxyz.com",
+      "activo": true
+    },
+    {
+      "id": 2,
+      "nit": "987654321",
+      "nombre": "Compañía ABC Ltda.",
+      "direccion": "Avenida Siempre Viva 742",
+      "ciudad": "Springfield",
+      "pais": "EE.UU.",
+      "telefono": "098-765-4321",
+      "correoCorporativo": "info@companiaabc.com",
+      "activo": false
+    }
+  ];
+  
+  module.exports = clientes;
+  
--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const activities=require("./data/Activities")
 const opportunities=require("./data/Opportunities")
+const clientes = require("./data/Client")
+
 module.exports = () => {
     return {
         activities,
         opportunities,
+        clientes,
     }
 
 }
