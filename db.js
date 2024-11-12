9dab65dc439f2ff3ff00f0b2afaa9198cd8438ef
Kacueto <75464308+Kacueto@users.noreply.github.com>
2024-11-12 13:08:19 +0000

    change js to json

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 // db.js
 const activities = require("./data/Activities");
 const opportunities = require("./data/Opportunities");
-const clientes = require("./data/Client");
+const clientes = require("./data/Client.json");
 
 module.exports = () => {
   return {
